3c7dadb86c4cf77bfc25b5095fc373829536b22b
Isabell Cowan <izzi@izzette.com>
2019-08-13 21:55:17 -0500

    Broke main.c apart into static functions, other improvements in main.c.

--- main.c
+++ main.c
@@ -25,16 +25,59 @@
 #define PING_TTL (64)
 #define PING_MAX_CONSECURIVE_MISSED_COUNT (1)
 
-static uint16_t ping_id;
-static struct ping_record *ping_record;
+struct monitor_ip_config {
+	const struct sockaddr *ping_addr;
+	struct netio_params netio_params;
+	struct monitor_params monitor_params;
+	struct timespec time_ping_delay;
+	struct timespec time_ping_expire;
+};
 
-__attribute__((constructor))
-static void set_ping_id () {
-	uint32_t pid = getpid();
-	ping_id = (pid >> 16) ^ (pid & 0xffff);
+static uint16_t monitor_ip_get_ping_id ();
+static void monitor_ip_usage (const char *);
+static struct monitor_ip_config *monitor_ip_configure (
+		int, const char *[]);
+static void monitor_ip_config_free (struct monitor_ip_config *);
+static int monitor_ip_open_socket (sa_family_t);
+static void monitor_ip_set_socket_options (sa_family_t, int);
+static void monitor_ip_update_pongs (
+		struct ping_record *, size_t, const struct netio_pong *);
+static void monitor_ip_loop_iter (
+		int, struct monitor_ip_config *, struct ping_record *, unsigned long long *);
+
+int main(int argc, const char *argv[])
+{
+	// Get config.
+	struct monitor_ip_config *cfg = monitor_ip_configure(argc, argv);
+
+	// Open socket.
+	int ping_sockfd = monitor_ip_open_socket(cfg->ping_addr->sa_family);
+
+	// Set socket options.
+	monitor_ip_set_socket_options(cfg->ping_addr->sa_family, ping_sockfd);
+
+	struct ping_record *ping_record = ping_record_init(&cfg->time_ping_expire);
+
+	unsigned long long pings_sent = 0;
+	do {
+		monitor_ip_loop_iter(ping_sockfd, cfg, ping_record, &pings_sent);
+	} while (1);
+
+	ping_record_free(ping_record);
+
+	monitor_ip_config_free(cfg);
+
+	return 0;
 }
 
-static void usage(const char *progname) {
+__attribute__((constructor))
+static uint16_t monitor_ip_get_ping_id () {
+	uint32_t pid = getpid();
+	uint16_t ping_id = (pid >> 16) ^ (pid & 0xffff);
+	return ping_id;
+}
+
+static void monitor_ip_usage (const char *progname) {
 	fprintf(stderr, "Usage: %s [-4|-6] <address> [<hook command>]\n", progname);
 	fprintf(stderr, "\n");
 	fprintf(stderr, "Options:\n");
@@ -42,9 +85,9 @@ static void usage(const char *progname) {
 	fprintf(stderr, "       -6      Interpret <address> as an IPv6 address.\n");
 }
 
-int main(int argc, const char *argv[])
-{
-	int ping_sockfd;
+static struct monitor_ip_config *monitor_ip_configure (
+		int argc, const char *argv[]
+) {
 	struct sockaddr *ping_addr;
 
 	const char *progname;
@@ -55,7 +98,7 @@ int main(int argc, const char *argv[])
 
 	if (4 != argc) {
 		fprintf(stderr, "Incorrect arguments!\n");
-		usage(progname);
+		monitor_ip_usage(progname);
 		exit(1);
 	}
 
@@ -63,133 +106,185 @@ int main(int argc, const char *argv[])
 	const char *ip_address_str = argv[2];
 	const char *monitor_notify_cmd = argv[3];
 
-	struct sockaddr_in ping_addr4;
-	struct sockaddr_in6 ping_addr6;
 	if (0 == strcmp("-4", af_inet_version)) {
-		int parse_result = inet_pton(AF_INET, ip_address_str, &ping_addr4.sin_addr);
+		struct sockaddr_in *ping_addr4 = malloc(sizeof(struct sockaddr_in));
+		int parse_result = inet_pton(AF_INET, ip_address_str, &ping_addr4->sin_addr);
 		if (1 == parse_result) {
-			ping_addr4.sin_family = AF_INET;
-			ping_addr4.sin_port = PING_PORT;
-			ping_addr = (struct sockaddr *)&ping_addr4;
+			ping_addr4->sin_family = AF_INET;
+			ping_addr4->sin_port = PING_PORT;
+			ping_addr = (struct sockaddr *)ping_addr4;
 		} else if (0 == parse_result) {
 			fprintf(stderr, "Invalid IPv4 address\n");
-			return 1;
+			exit(1);
 		} else {
 			fprintf(stderr, "Failed to parse IPv4 address: %s\n", strerror(errno));
-			return 1;
+			exit(1);
 		}
 	} else if (0 == strcmp("-6", af_inet_version)) {
-		int parse_result = inet_pton(AF_INET6, ip_address_str, &ping_addr6.sin6_addr);
+		struct sockaddr_in6 *ping_addr6 = malloc(sizeof(struct sockaddr_in6));
+		int parse_result = inet_pton(
+				AF_INET6, ip_address_str, &ping_addr6->sin6_addr);
 		if (1 == parse_result) {
-			ping_addr6.sin6_family = AF_INET6;
-			ping_addr6.sin6_port = PING_PORT;
-			ping_addr = (struct sockaddr *)&ping_addr6;
+			ping_addr6->sin6_family = AF_INET6;
+			ping_addr6->sin6_port = PING_PORT;
+			ping_addr = (struct sockaddr *)ping_addr6;
 		} else if (0 == parse_result) {
 			fprintf(stderr, "Invalid IPv6 address\n");
-			return 1;
+			exit(1);
 		} else {
 			fprintf(stderr, "Failed to parse IPv6 address: %s\n", strerror(errno));
-			return 1;
+			exit(1);
 		}
 	} else {
-		fprintf(stderr, "Unknown address family!\n");
-		usage(progname);
-		return 1;
-	}
-
-
-	if (ping_addr->sa_family == AF_INET) {
-		ping_sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-	} else {
-		ping_sockfd = socket(AF_INET6, SOCK_RAW, IPPROTO_ICMPV6);
-	}
-	if (0 > ping_sockfd) {
-		fprintf(stderr, "Error creating socket: %s\n", strerror(errno));
-		return 1;
-	}
-
-	// Set socket options.
-	if (ping_addr->sa_family == AF_INET) {
-		int ttl_val = PING_TTL;
-		if (setsockopt(ping_sockfd, SOL_IP, IP_TTL, &ttl_val, sizeof(ttl_val)) != 0) {
-			fprintf(stderr, "Failed setting socket TTL: %s\n", strerror(errno));
-			return 1;
-		}
+		fprintf(stderr, "Unknown address family specified by: %s\n", af_inet_version);
+		monitor_ip_usage(progname);
+		exit(1);
 	}
 
 	struct netio_params netio_params = {
 		.msg_s = PING_MSG_S,
-		.id = ping_id
+		.id = monitor_ip_get_ping_id()
 	};
+
+	size_t monitor_notify_cmd_s = strlen(monitor_notify_cmd) + 1;
+	char *monitor_notify_cmd_permenant = malloc(monitor_notify_cmd_s);
+	memcpy(monitor_notify_cmd_permenant, monitor_notify_cmd, monitor_notify_cmd_s);
 
 	struct monitor_params monitor_params = {
 		.block = true,
 		.missed_max = PING_MAX_CONSECURIVE_MISSED_COUNT,
-		.notify_command = monitor_notify_cmd
+		.notify_command = monitor_notify_cmd_permenant
 	};
-
-	struct timespec timeout_ping_expire;
-	useconds2timespec(TIMEOUT_PING_EXPIRE_US, &timeout_ping_expire);
-	ping_record = ping_record_init(&timeout_ping_expire);
 
 	struct timespec time_ping_delay;
 	useconds2timespec(TIME_PING_DELAY_US, &time_ping_delay);
 
-	uint64_t pings_sent = 0;
-	do {
-		struct timespec time_ping_start;
-		clock_gettime(CLOCK_MONOTONIC, &time_ping_start);
+	struct timespec timeout_ping_expire;
+	useconds2timespec(TIMEOUT_PING_EXPIRE_US, &timeout_ping_expire);
 
-		// sequence = pings_sent % (1 << 16) .
-		uint16_t sequence = pings_sent;
-		struct ping_record_entry ping_record_entry;
-		netio_send(
-				&netio_params, ping_sockfd, ping_addr, sequence, &ping_record_entry);
+	struct monitor_ip_config *cfg = malloc(sizeof(struct monitor_ip_config));
+	*cfg = (struct monitor_ip_config){
+		.monitor_params = monitor_params,
+		.netio_params = netio_params,
+		.ping_addr = ping_addr,
+		.time_ping_delay = time_ping_delay,
+		.time_ping_expire = timeout_ping_expire
+	};
 
-		ping_record_submit(ping_record, &ping_record_entry);
-		pings_sent += 1;
+	return cfg;
+}
 
-		struct timespec time_recv_end;
-		timespec_add(&time_ping_start, &time_ping_delay, &time_recv_end);
+static void monitor_ip_config_free (struct monitor_ip_config *cfg) {
+	free((void *)cfg->ping_addr);
+	free((void *)cfg->monitor_params.notify_command);
+	free(cfg);
+}
 
-		// MUST FREE pongs
-		struct netio_pong *pongs;
-		size_t pongs_s = netio_receive(&netio_params, ping_sockfd, ping_addr,
-				&time_recv_end, &pongs);
+static int monitor_ip_open_socket (sa_family_t af) {
+	int ping_sockfd;
+	switch (af) {
+	case AF_INET:
+		ping_sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+		break;
+	case AF_INET6:
+		ping_sockfd = socket(AF_INET6, SOCK_RAW, IPPROTO_ICMPV6);
+		break;
+	default:
+		fprintf(stderr, "Unknown address family: %d\n", (int)af);
+		exit(1);
+	}
 
-		struct timespec time_since_last_receive;
-		clock_gettime(CLOCK_MONOTONIC, &time_since_last_receive);
+	if (0 > ping_sockfd) {
+		fprintf(stderr, "Error creating socket: %s\n", strerror(errno));
+		exit(1);
+	}
 
-		for (size_t i = 0; pongs_s > i; ++i) {
-			struct netio_pong pong = pongs[i];
+	return ping_sockfd;
+}
 
-			struct ping_record_entry entry_data;
-			if (!ping_record_update_pong(ping_record, pong.seq, &entry_data)) {
-				fprintf(stderr,
-						"Pong for sequence %hu received, but not in sent record, possibly "
-						"expired or errant?\n", pong.seq);
-				continue;
-			}
+static void monitor_ip_set_socket_options (sa_family_t af, int ping_sockfd) {
+	static const int ttl_val = PING_TTL;
 
-			struct timespec time_rtt;
-			timespec_diff(&entry_data.time_sent, &pong.time_recv, &time_rtt);
+	switch (af) {
+	case AF_INET:
+		if (setsockopt(ping_sockfd, SOL_IP, IP_TTL, &ttl_val, sizeof(ttl_val)) != 0) {
+			fprintf(stderr, "Failed setting socket TTL: %s\n", strerror(errno));
+			exit(1);
+		}
+		break;
+	case AF_INET6:
+		// TODO
+		{}
+		break;
+	default:
+		fprintf(stderr, "Unknown address family: %d\n", (int)af);
+		exit(1);
+	}
+}
 
-			const char *is_dup_str = (1 < entry_data.pong_cnt) ? " (dup)" : "";
-			const char *is_exp_str =
-				(1 > cmp_timespec(&timeout_ping_expire, &time_rtt)) ? " (expired)" : "";
-			fprintf(stdout, "Pong for sequence %hu%s, rtt: %.02fms%s\n",
-				entry_data.sequence, is_dup_str, timespec2double(&time_rtt) * 1000.,
-				is_exp_str);
+static void monitor_ip_update_pongs (
+		struct ping_record *ping_record, size_t pongs_s,
+		const struct netio_pong *pongs
+) {
+	for (size_t i = 0; pongs_s > i; ++i) {
+		struct netio_pong pong = pongs[i];
+
+		struct ping_record_entry entry_data;
+		if (!ping_record_update_pong(ping_record, pong.seq, &entry_data)) {
+			fprintf(stderr,
+					"Pong for sequence %hu received, but not in sent record, possibly "
+					"expired or errant?\n", pong.seq);
+			continue;
 		}
 
-		free(pongs);
+		struct timespec time_rtt;
+		timespec_diff(&entry_data.time_sent, &pong.time_recv, &time_rtt);
 
-		ping_record_collect_expired(ping_record, &time_since_last_receive);
+		const char *is_dup_str = (1 < entry_data.pong_cnt) ? " (dup)" : "";
+		const char *is_exp_str =
+			(1 > cmp_timespec(&ping_record->timeout, &time_rtt)) ? " (expired)" : "";
+		fprintf(stdout, "Pong for sequence %hu%s, rtt: %.02fms%s\n",
+			entry_data.sequence, is_dup_str, timespec2double(&time_rtt) * 1000.,
+			is_exp_str);
+	}
+}
 
-		test_monitor_notify_trigger(&monitor_params, ping_record->missed_cnt);
-	} while (1);
+static void monitor_ip_loop_iter (
+		int ping_sockfd, struct monitor_ip_config *cfg,
+		struct ping_record *ping_record, unsigned long long *pings_sent
+) {
+	struct timespec time_ping_start;
+	clock_gettime(CLOCK_MONOTONIC, &time_ping_start);
 
-	ping_record_free(ping_record);
+	// sequence = pings_sent % (1 << 16) .
+	uint16_t sequence = (*pings_sent)++;
+	struct ping_record_entry ping_record_entry;
+	netio_send(
+			&cfg->netio_params, ping_sockfd, cfg->ping_addr,
+			sequence, &ping_record_entry);
 
-	return 0;
+	ping_record_submit(ping_record, &ping_record_entry);
+
+	struct timespec time_recv_end;
+	timespec_add(
+			&time_ping_start, &cfg->time_ping_delay, &time_recv_end);
+
+	// MUST FREE pongs
+	struct netio_pong *pongs;
+	size_t pongs_s = netio_receive(
+			&cfg->netio_params, ping_sockfd, cfg->ping_addr,
+			&time_recv_end, &pongs);
+
+	struct timespec time_since_last_receive;
+	clock_gettime(CLOCK_MONOTONIC, &time_since_last_receive);
+
+	monitor_ip_update_pongs(ping_record, pongs_s, pongs);
+
+	// FREED pongs
+	free(pongs);
+
+	ping_record_collect_expired(ping_record, &time_since_last_receive);
+
+	test_monitor_notify_trigger(
+			&cfg->monitor_params, ping_record->missed_cnt);
 }
